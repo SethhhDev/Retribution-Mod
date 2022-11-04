db25d84aef91bebd8ee4aa3a32d735100976076e
SethhhDev <81801587+SethhhDev@users.noreply.github.com>
2022-11-03 21:47:53 -0500

    Add files via upload

---
+++ Items/Weapons/GlisteningSludgeStaff.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using RetributionMod.Projectiles;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RetributionMod.Items.Weapons
+{
+    internal class GlisteningSludgeStaff : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Glistening Sludge Staff");
+            Tooltip.SetDefault("Summons a Gistening Sludge to fight for you");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 18;
+            Item.knockBack = 4f;
+            Item.mana = 10;
+            Item.width = 32;
+            Item.height = 32;
+            Item.useTime = 36;
+            Item.useAnimation = 36;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.value = Item.buyPrice(0, 50, 0, 0);
+            Item.rare = ItemRarityID.Blue;
+            Item.UseSound = SoundID.Item44;
+
+            Item.noMelee = true;
+            Item.buffType = ModContent.BuffType<Buffs.GlisteningSludge>();
+            Item.shoot = ModContent.ProjectileType<Projectiles.Minions.GlisteningSludge>();
+            Item.DamageType = DamageClass.Summon;
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            player.AddBuff(Item.buffType, 2);
+
+            position = Main.MouseWorld;
+            return true;
+        }
+    }
+}
---
+++ Items/Weapons/GlisteningSpear.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using RetributionMod.Projectiles;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RetributionMod.Items.Weapons
+{
+    internal class GlisteningSpear : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Glistening Spear");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 12;
+            Item.crit = 32;
+            Item.useStyle = ItemUseStyleID.HoldUp;
+            Item.useAnimation = 23;
+            Item.useTime = 29;
+            Item.shootSpeed = 8.3f;
+            Item.knockBack = 3.8f;
+            Item.width = 32;
+            Item.height = 32;
+            Item.scale = 1f;
+            Item.rare = ItemRarityID.Blue;
+            Item.value = Item.sellPrice(silver: 55);
+
+            Item.noMelee = true; 
+            Item.noUseGraphic = true; 
+            Item.autoReuse = true; 
+
+            Item.UseSound = SoundID.Item29;
+            Item.shoot = ModContent.ProjectileType<GlisteningSpearProjectile>();
+
+            Item.DamageType = DamageClass.Melee;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            return player.ownedProjectileCounts[Item.shoot] < 1;
+        }
+    }
+}
