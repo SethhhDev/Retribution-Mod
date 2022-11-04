56f3a86ccdb8328f656a280693b24b7036df5585
SethhhDev <81801587+SethhhDev@users.noreply.github.com>
2022-11-03 21:48:30 -0500

    Add files via upload

---
+++ Projectiles/GlisteningSpearProjectile.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Xna.Framework;
+using RetributionMod.Projectiles;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RetributionMod.Projectiles
+{
+    internal class GlisteningSpearProjectile : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Glistening Spear");
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 18;
+            Projectile.height = 18;
+            Projectile.aiStyle = 19;
+            Projectile.penetrate = -1;
+            Projectile.scale = 1.1f;
+            Projectile.alpha = 0;
+
+            Projectile.hide = true;
+            Projectile.ownerHitCheck = true;
+            Projectile.tileCollide = false;
+            Projectile.friendly = true;
+        }
+
+        
+        public float movementFactor
+        {
+            get => Projectile.ai[0];
+            set => Projectile.ai[0] = value;
+        }
+
+        public override void AI()
+        {
+
+            Player projOwner = Main.player[Projectile.owner];
+          
+            Vector2 ownerMountedCenter = projOwner.RotatedRelativePoint(projOwner.MountedCenter, true);
+            Projectile.direction = projOwner.direction;
+            projOwner.heldProj = Projectile.whoAmI;
+            projOwner.itemTime = projOwner.itemAnimation;
+            Projectile.position.X = ownerMountedCenter.X - (float)(Projectile.width / 2);
+            Projectile.position.Y = ownerMountedCenter.Y - (float)(Projectile.height / 2);
+
+            if (!projOwner.frozen)
+            {
+                if (movementFactor == 0f)
+                {
+                    movementFactor = 0.4f; 
+                    Projectile.netUpdate = true; 
+                }
+                if (projOwner.itemAnimation < projOwner.itemAnimationMax / 3) 
+                {
+                    movementFactor -= 1.6f;
+                }
+                else 
+                {
+                    movementFactor += 1.3f;
+                }
+            }
+            Projectile.position += Projectile.velocity * movementFactor;
+            if (projOwner.itemAnimation == 0)
+            {
+                Projectile.Kill();
+            }
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.ToRadians(135f);
+            if (Projectile.spriteDirection == -1)
+            {
+                Projectile.rotation -= MathHelper.ToRadians(90f);
+            }
+
+            if (Main.rand.NextBool(3))
+            {
+                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.height, Projectile.width, DustID.PurpleCrystalShard,
+                    Projectile.velocity.X * .2f, Projectile.velocity.Y * .2f, 200, Scale: 1.2f);
+                dust.velocity += Projectile.velocity * 0.3f;
+                dust.velocity *= 0.2f;
+
+                dust = Dust.NewDustDirect(Projectile.position, Projectile.height, Projectile.width, DustID.BlueCrystalShard,
+                   Projectile.velocity.X * .2f, Projectile.velocity.Y * .2f, 200, Scale: 1.2f);
+                dust.velocity += Projectile.velocity * 0.3f;
+                dust.velocity *= 0.2f;
+            }
+            if (Main.rand.NextBool(4))
+            {
+                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.height, Projectile.width, DustID.PurpleCrystalShard,
+                    0, 0, 254, Scale: 0.3f);
+                dust.velocity += Projectile.velocity * 0.5f;
+                dust.velocity *= 0.5f;
+
+                dust = Dust.NewDustDirect(Projectile.position, Projectile.height, Projectile.width, DustID.BlueCrystalShard,
+                    0, 0, 254, Scale: 0.3f);
+                dust.velocity += Projectile.velocity * 0.5f;
+                dust.velocity *= 0.5f;
+
+            }
+        }
+    }
+}
---
+++ Projectiles/Minions/GlisteningSludge.cs
@@ -0,0 +1,197 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using System;
+
+namespace RetributionMod.Projectiles.Minions
+{
+    internal class GlisteningSludge : ModProjectile
+    {
+        private const int AI_State_Slot = 0;
+        private const int State_Asleep = 0;
+        private const int State_Notice = 1;
+        private const int State_Jump = 2;
+        private const int State_Hover = 3;
+        private const int State_Fall = 4;
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Projectile.type] = 3;
+            Main.projPet[Projectile.type] = true;
+            ProjectileID.Sets.MinionSacrificable[Projectile.type] = true;
+            ProjectileID.Sets.MinionTargettingFeature[Projectile.type] = true; 
+        }
+        public override void SetDefaults()
+        {
+            Projectile.width = 32;
+            Projectile.height = 32;
+            Projectile.aiStyle = -1; // This npc has a completely unique AI, so we set this to -1. The default aiStyle 0 will face the player, which might conflict with custom AI code.
+            Projectile.damage = 7;
+            Projectile.friendly = true;
+            Projectile.minion = true;
+            Projectile.minionSlots = 1;
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 18000;
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
+
+        }
+
+        public override bool MinionContactDamage()
+        {
+            return true;
+        }
+        public float AI_State
+        {
+            get => Projectile.ai[AI_State_Slot];
+            set => Projectile.ai[AI_State_Slot] = value;
+        }
+
+        public override void AI()
+        {
+            Player player = Main.player[Projectile.owner];
+
+            #region Active check
+
+            if (player.dead || !player.active)
+            {
+                player.ClearBuff(ModContent.BuffType<Buffs.GlisteningSludge>());
+            }
+            if (player.HasBuff(ModContent.BuffType<Buffs.GlisteningSludge>()))
+            {
+                Projectile.timeLeft = 2;
+            }
+            #endregion
+
+            #region General behavior
+            Vector2 idlePosition = player.Center;
+
+            // If your minion doesn't aimlessly move around when it's idle, you need to "put" it into the line of other summoned minions
+            // The index is Projectile.minionPos
+            float minionPositionOffsetX = (10 + Projectile.minionPos * 40) * -player.direction;
+            idlePosition.X += minionPositionOffsetX; // Go behind the player
+
+            // All of this code below this line is adapted from Spazmamini code (ID 388, aiStyle 66)
+
+            // Teleport to player if distance is too big
+            Vector2 vectorToIdlePosition = idlePosition - Projectile.Center;
+            float distanceToIdlePosition = vectorToIdlePosition.Length();
+            if (Main.myPlayer == player.whoAmI && distanceToIdlePosition > 2000f)
+            {
+                // Whenever you deal with non-regular events that change the behavior or position drastically, make sure to only run the code on the owner of the Projectile,
+                // and then set netUpdate to true
+                Projectile.position = idlePosition;
+                Projectile.velocity.X *= 0.1f;
+                Projectile.netUpdate = true;
+            }
+
+            #endregion
+
+            #region Find target
+            // Starting search distance
+            float distanceFromTarget = 700f;
+            Vector2 targetCenter = Projectile.position;
+            bool foundTarget = false;
+
+            // This code is required if your minion weapon has the targeting feature
+            if (player.HasMinionAttackTargetNPC)
+            {
+                NPC npc = Main.npc[player.MinionAttackTargetNPC];
+                float between = Vector2.Distance(npc.Center, Projectile.Center);
+                // Reasonable distance away so it doesn't target across multiple screens
+                if (between < 2000f)
+                {
+                    distanceFromTarget = between;
+                    targetCenter = npc.Center;
+                    foundTarget = true;
+                }
+            }
+            if (!foundTarget)
+            {
+                // This code is required either way, used for finding a target
+                for (int i = 0; i < Main.maxNPCs; i++)
+                {
+                    NPC npc = Main.npc[i];
+                    if (npc.CanBeChasedBy())
+                    {
+                        float between = Vector2.Distance(npc.Center, Projectile.Center);
+                        bool closest = Vector2.Distance(Projectile.Center, targetCenter) > between;
+                        bool inRange = between < distanceFromTarget;
+                        bool lineOfSight = Collision.CanHitLine(Projectile.position, Projectile.width, Projectile.height, npc.position, npc.width, npc.height);
+                        // Additional check for this specific minion behavior, otherwise it will stop attacking once it dashed through an enemy while flying though tiles afterwards
+                        // The number depends on various parameters seen in the movement code below. Test different ones out until it works alright
+                        bool closeThroughWall = between < 100f;
+                        if (((closest && inRange) || !foundTarget) && (lineOfSight || closeThroughWall))
+                        {
+                            distanceFromTarget = between;
+                            targetCenter = npc.Center;
+                            foundTarget = true;
+                        }
+                    }
+                }
+            }
+
+            // friendly needs to be set to true so the minion can deal contact damage
+            // friendly needs to be set to false so it doesn't damage things like target dummies while idling
+            // Both things depend on if it has a target or not, so it's just one assignment here
+            // You don't need this assignment if your minion is shooting things instead of dealing contact damage
+            Projectile.friendly = foundTarget;
+            #endregion
+
+            #region Movement
+            float speed = 8f;
+            float inertia = 20f;
+
+            if (foundTarget)
+            {
+                // Minion has a target: attack (here, fly towards the enemy)
+                if (distanceFromTarget > 40f)
+                {
+                    // The immediate range around the target (so it doesn't latch onto it when close)
+                    Vector2 direction = targetCenter - Projectile.Center;
+                    direction.Normalize();
+                    direction *= speed;
+                    Projectile.velocity = (Projectile.velocity * (inertia - 1) + direction) / inertia;
+                }
+            }
+            else
+            {
+                // Minion doesn't have a target: return to player and idle
+                if (distanceToIdlePosition > 600f)
+                {
+                    // Speed up the minion if it's away from the player
+                    speed = 12f;
+                    inertia = 60f;
+                }
+                else
+                {
+                    // Slow down the minion if closer to the player
+                    speed = 4f;
+                    inertia = 80f;
+                }
+                if (distanceToIdlePosition > 20f)
+                {
+                    // The immediate range around the player (when it passively floats about)
+
+                    // This is a simple movement formula using the two parameters and its desired direction to create a "homing" movement
+                    vectorToIdlePosition.Normalize();
+                    vectorToIdlePosition *= speed;
+                    Projectile.velocity = new Vector2(player.velocity.X / vectorToIdlePosition.X, 0);
+                }
+                else if (Projectile.velocity == Vector2.Zero)
+                {
+                    // If there is a case where it's not moving at all, give it a little "poke"
+                    Projectile.velocity.X = -0.15f;
+                }
+            }
+                    #endregion
+        }
+
+        public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac)
+        {
+            fallThrough = false;
+            return true;
+        }
+
+    }
+}
